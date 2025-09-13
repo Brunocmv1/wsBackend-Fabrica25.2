a7107f2d9b2f8dd29da54d3034d2337ce393aa53
Brunocmv1 <brunocmv1@gmail.com>
2025-09-13 09:17:42 -0300

    ✨ feat: Define and Migrate Film model

--- app/models.py
+++ app/models.py
@@ -7,4 +7,12 @@ class Ator(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=200)
+    ano_lancamento = models.IntegerField()
+    sinopse = models.TextField()
+    atores = models.ManyToManyField(Ator, blank=True)
+
+    def __str__(self):
+        return self.titulo
