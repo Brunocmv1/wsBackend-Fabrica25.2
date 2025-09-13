4548147209abdd4fc10c00dd68889ddb59dff0af
Brunocmv1 <brunocmv1@gmail.com>
2025-09-13 00:36:32 -0300

    ✨ feat: I Implement Actor Update Functionality

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home, CreateAtores, ListAtores
+from .views import Home, CreateAtores, ListAtores, UpdateAtores
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('criarAtores', CreateAtores.as_view(), name="create_a"),
     path('listarAtores', ListAtores.as_view(), name="list_a" ),
+    path('atualizarAtores/<int:pk>', UpdateAtores.as_view(), name="update_a"),
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView
 from .models import Ator
 from .forms import AtorForm
 
@@ -18,3 +18,10 @@ class ListAtores(ListView):
     model = Ator
     template_name = "listAtores.html"
     context_object_name = "atores"
+
+class UpdateAtores(UpdateView):
+    model = Ator
+    form_class = AtorForm
+    template_name = "createAtores.html"
+    success_url = reverse_lazy("list_a")
+
